69dbabdc86df17d95df908ad587e8fe95f3d81e6
VladimirS <v.a.samarin@yandex.ru>
2023-11-30 18:16:44 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,36 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      if (action.payload === "up") {
+        const sortedState = state.sort((a, b) => a.name.localeCompare(b.name));
+        console.log(sortedState);
+        return sortedState;
+      }
+      if (action.payload === "down") {
+        const sortedState = state
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .reverse();
+        console.log(sortedState);
+        return sortedState;
+      }
+      return state; // need to fix
     }
-}
+    case "check": {
+      const sortedState = state.filter((a) => a.age >= 18);
+      return sortedState; // need to fix
+    }
+    default:
+      return state;
+  }
+};
